97c180ae9119f28f1cdd4431e2b378251588bb1c
bekzhan43 <bekzha4334@gmail.com>
2022-10-17 17:51:10 +0600

    fixed bugs

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -5,5 +5,5 @@ from loader import dp
 
 
 @dp.message_handler(CommandStart())
-async def bot_start(message:types.Message):
+async def bot_start(message: types.Message):
     await message.answer(f"Hi, {message.from_user.full_name}!")
